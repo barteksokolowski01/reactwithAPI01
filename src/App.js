8b60f5cf6ee838cf7a89309e41ce4e028ce12ff1
s24531 <s24531@pjwstk.edu.pl>
2023-12-07 19:16:05 +0100

    create API app with randomuser.me

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  const fetchRandomUser = async () => {
+    try {
+      const response = await fetch('https://randomuser.me/api/');
+      const data = await response.json();
+      setUserData(data.results[0]);
+    } catch (error) {
+      console.error('Error fetching random user data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Random User Generator</h1>
+          <button onClick={fetchRandomUser}>Generate Random User</button>
+          {userData && (
+              <div className="user-info">
+                <img src={userData.picture.large} alt="User" />
+                <p>Name: {`${userData.name.title} ${userData.name.first} ${userData.name.last}`}</p>
+                <p>Email: {userData.email}</p>
+                <p>Location: {`${userData.location.city}, ${userData.location.country}`}</p>
+                <p>Phone: {userData.phone}</p>
+              </div>
+          )}
+        </header>
+      </div>
   );
 }
 
